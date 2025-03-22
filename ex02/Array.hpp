0e5af7acea3070ce6bc82c208db5acfac5a43c46
RinoNaito <rinodelevingne@gmail.com>
2025-03-22 16:56:53 +0900

    NOTHING: to continue with  Linux

--- ex02/Array.hpp
+++ ex02/Array.hpp
@@ -1,16 +1,61 @@
 #pragma once
 
+#include <iostream>
+#include <cstddef>
+#include <stdexcept>
+
 template <typename T>
 class Array {
 	public:
-		Array();
-		Array(unsigned int n);
-		Array(Array const &other);
-		Array &operator=(Array const &other);
-		~Array();
-		size_t size() const;
-		T &operator[](Array const &other);
+		Array(): size_(0){
+			this->array_ = new T[this->size_];
+		};
+		Array(unsigned int size) : size_(size){
+			this->array_ = new T[this->size_];
+		};
+		~Array(){
+			delete[] array_;	
+		};
+		Array(Array const &other){
+			*this = other;
+		};
+		Array &operator=(Array const &other){
+			if (this->array_ != NULL){
+				delete[] array_;
+			}
+			this->size_ = other.size();
+			this->array_ = new T[this->size_];
+			for (size_t index = 0; index < this->size_; index++){
+				this->array_[index] = other.array_[index];
+			}
+			return *this;
+		};
+		size_t size() const{
+			return this->size_;
+		};
+		T &operator[](size_t index){
+			if (index < 0 || index >= this->size_
+				|| this->array_ == NULL || this->array_.size() == 0){
+				throw new IndexOutOfRangeException();
+			}
+			return array_[index];
+		};
 
 	private:
-		T array_[];
+		T *array_;
+		size_t size_;
+		class IndexOutOfRangeException : public std::runtime_error{
+			public:
+				IndexOutOfRangeException() :
+					runtime_error("the given index is out of range"){};
+		};
 };
+
+template <typename T>
+std::ostream& operator<<(std::ostream &os, Array<T> const &array){
+	os << "[Array]:  - size: " << array.size() << "\n";
+	for (size_t index = 0; index < array.size(); index++){
+		os << "\t[" << index << "]: " << array[index] << "\n";
+	}
+	return os;
+}
---
+++ ex02/main.cpp
@@ -0,0 +1,74 @@
+#include "Array.hpp"
+
+void PrintWithBlueColor(std::string message)
+{
+	const std::string kBulue = "\033[36m";
+	const std::string kReset = "\033[0m";
+	std::cout << kBulue << message << kReset << std::endl; 
+}
+
+static Array<int>	&createTestArrayForInt(size_t size){
+	Array<int> *array = new Array<int>(size);
+	int elem = 0;
+	for (size_t index = 0; index < size; index++){
+		array[index] = elem;
+		elem += 5;
+	}
+	return *array;
+}
+
+static Array<char>	&createTestArrayForChar(size_t size){
+	Array<char> *array = new Array<char>(size);
+	char elem = 'a';
+	for (size_t index = 0; index < size; index++){
+		array[index] = elem;
+		elem += 5;
+	}
+	return *array;
+}
+
+static Array<double>	&createTestArrayForDouble(size_t size){
+	Array<double> *array = new Array<double>(size);
+	double elem = 5.5;
+	for (size_t index = 0; index < size; index++){
+		array[index] = elem;
+		elem += 1.1;
+	}
+	return *array;
+}
+
+int main(void){
+	const size_t sizeForArrayInt = 5;
+	const size_t sizeForArrayChar = 7;
+	const size_t sizeForArrayDouble = 3;
+	Array<int> arrayInt = createTestArrayForInt(sizeForArrayInt);
+	Array<char> arrayChar = createTestArrayForChar(sizeForArrayChar);
+	Array<double> arrayDouble = createTestArrayForDouble(sizeForArrayDouble);
+	
+	PrintWithBlueColor("< After creating Arrays >");
+	PrintWithBlueColor("- arrayInt");
+	std::cout << arrayInt << std::endl;
+	PrintWithBlueColor("- arrayChar");
+	std::cout << arrayChar << std::endl;
+	PrintWithBlueColor("- arrayDouble");
+	std::cout << arrayDouble << std::endl;
+
+	PrintWithBlueColor("< After assigning arrayChar to arrayChar2 >");
+	Array<char> arrayChar2 = arrayChar;
+	PrintWithBlueColor("- arrayChar2");
+	std::cout << arrayChar2 << std::endl;
+	
+	PrintWithBlueColor("< After constructing arrayChar2 from arrayChar >");
+	Array<char> arrayChar3 = Array<char>(arrayChar2);
+	PrintWithBlueColor("- arrayChar3");
+	std::cout << arrayChar3 << std::endl;
+	
+	PrintWithBlueColor("< After changing arrayChar3[2] >");
+	arrayChar3[2] = '2';
+	PrintWithBlueColor("- arrayChar");
+	std::cout << arrayChar << std::endl;
+	PrintWithBlueColor("- arrayChar2");
+	std::cout << arrayChar2 << std::endl;
+	PrintWithBlueColor("- arrayChar3");
+	std::cout << arrayChar3 << std::endl;
+}
